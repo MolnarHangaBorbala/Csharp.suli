7b6f7104eb8d812bf8b50ca508b501f0ddc54513
_zendev <molnarhanga4@gmail.com>
2026-01-07 11:59:46 +0100

    Create 2026-01-07.cs

---
+++ 2026-01-07.cs
@@ -0,0 +1,87 @@
+/*
+//Páros számok
+List<int> szamok = new List<int>{1, 2, 3, 4, 5, 6, 7, 8};
+List<int> parosszamok = new List<int>();
+
+foreach (int szam in szamok)
+{
+    if (szam % 2 == 0)
+    {
+        parosszamok.Add(szam);
+    }
+}
+Console.WriteLine("2-vel osztható számok: " + string.Join(", ", parosszamok));
+
+//Switch
+Console.WriteLine("Adj meg eg számot (1-7): ");
+int nap = int.Parse(Console.ReadLine());
+
+switch (nap)
+{
+    case 1:
+        Console.WriteLine("Hétfő");
+        break;
+    case 2:
+        Console.WriteLine("Kedd");
+        break;
+    case 3:
+        Console.WriteLine("Szerda");
+        break;
+    case 4:
+        Console.WriteLine("Csütörtök");
+        break;
+    case 5:
+        Console.WriteLine("Péntek");
+        break;
+    case 6:
+        Console.WriteLine("Szombat");
+        break;
+    case 7:
+        Console.WriteLine("Vasárnap");
+        break;
+    default:
+        Console.WriteLine("Nincs ilyen nap.");
+        break;
+}*/
+
+//1. feladat: Uszás
+Console.WriteLine("1. feladat: Kérem adja meg a heti aktivitást (MAX 250 karakter.): ");
+string aktivitas = Console.ReadLine();
+
+//2. feladat - távolság kiszámítása
+int osszKm = 0;
+bool voltU = false, voltG = false, voltF = false, voltK = false;
+
+foreach (char c in aktivitas)
+{
+    switch (c)
+    {
+        case 'U': osszKm += 1; voltU = true; break;
+        case 'G': osszKm += 1; voltG = true; break;
+        case 'F': osszKm += 2; voltF = true; break;
+        case 'K': osszKm += 10; voltK = true; break;
+    }
+}
+Console.WriteLine("2. feladat: A felhasználó által megtett távolság: {0} km", osszKm);
+int jutalom = 0;
+if (voltU && voltG && voltK && voltF)
+{
+    jutalom = 10;
+    Console.WriteLine("3. feladat: Bravo! Jutalma még 10 km.");
+}
+else
+{
+    Console.WriteLine("3. feladat: Nem jár jutalom.");
+}
+//4. feladat - végső teljesítmény + kiértékelés
+int vegsokm = osszKm + jutalom;
+Console.WriteLine("4. feladat: Összesített teljesítmény: {0} km", vegsokm);
+
+if (vegsokm >= 40)
+{
+    Console.WriteLine("Gratulálok, kihívás teljesítve!");
+}
+else
+{
+    Console.WriteLine("Nem sikerült a kihívás.");
+}
